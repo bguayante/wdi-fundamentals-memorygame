9a9654eb52ca92739b2f2b0932f7d270d8132fcf
Brian Guayante <brianguayante@Jens-MacBook-Air.local>
2020-03-12 16:46:22 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,19 @@ let cards = ["queen","queen","king","king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+ 		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+  		console.log("Sorry, try again.");
 	}
-}
+};
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
